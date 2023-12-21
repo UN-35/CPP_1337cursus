2989965f7487cf4e3c79dda49327dac4b18cfa69
Younes Elansari <yoelansa@e2r2p10.1337.ma>
2023-12-21 16:31:34 +0100

    cpp00 officially done :)

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -6,33 +6,25 @@
 /*   By: yoelansa <yoelansa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/20 17:16:24 by yoelansa          #+#    #+#             */
-/*   Updated: 2023/12/20 18:50:19 by yoelansa         ###   ########.fr       */
+/*   Updated: 2023/12/21 16:00:30 by yoelansa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-void PrintUpper( std::string str )
-{
-    char c = 0;
-    int i = -1;
-    while ( str[++i] ) {
-        if ( str[i] >= 'a' && str[i] <= 'z' ) {
-            c = str[i] - 32;
-            std::cout << c;
-        }
-        else
-            std::cout << str[i];
-    }
-}
-
 int main( int ac, char **av ) {
+    int i = 0;
+    int j;
     if ( ac == 1 )
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
     else {
-        int i = 0;
-        while ( ++i < ac )
-            PrintUpper( av[i] );
+        while ( ++i < ac ) {
+            j = -1;
+            while ( av[i][++j] ) {
+                av[i][j] = toupper(av[i][j]);
+            }
+            std::cout << av[i];
+        }
         std::cout << std::endl;
     }
 }
