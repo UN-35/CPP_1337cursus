44b69dbf69e5794dc02b927eb14b505b1a16893c
Younes Elansari <yoelansa@c3r5p3.1337.ma>
2024-02-04 16:13:13 +0100

    fix the ex04 in cpp01 by adding the .c_str() function which is a member func in the std::string class that returns a pointer to a null-terminated array..... the code wouldn't work without it by using the -std=c++98 flag

--- cpp01/ex04/Sed.cpp
+++ cpp01/ex04/Sed.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Sed.cpp                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: uns-35 <uns-35@student.42.fr>              +#+  +:+       +#+        */
+/*   By: yoelansa <yoelansa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 11:38:23 by uns-35            #+#    #+#             */
-/*   Updated: 2024/02/03 18:30:19 by uns-35           ###   ########.fr       */
+/*   Updated: 2024/02/04 16:05:36 by yoelansa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ Sed::~Sed()
 }
 
 void Sed::replace( std::string s1, std::string s2 ) {
-    std::ifstream inf(this->Infile);
+    std::ifstream inf(this->Infile.c_str());
     if ( !inf.is_open() ) {
         std::cerr << "open() failed" << std::endl;
         return ;
@@ -50,7 +50,7 @@ void Sed::replace( std::string s1, std::string s2 ) {
         i += s2.length();
     }
 
-    std::ofstream outf(this->Outfile);
+    std::ofstream outf(this->Outfile.c_str());
     outf << buffer;
     inf.close();
     outf.close();
