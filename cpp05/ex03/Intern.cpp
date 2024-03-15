8ffe823be037f87af25052b0a518dc680011b7e4
UN35 <younesari01@gmail.com>
2024-03-15 03:02:08 +0000

    .

--- cpp05/ex03/Intern.cpp
+++ cpp05/ex03/Intern.cpp
@@ -6,18 +6,53 @@
 /*   By: yoelansa <yoelansa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 13:20:34 by yoelansa          #+#    #+#             */
-/*   Updated: 2024/03/13 16:06:06 by yoelansa         ###   ########.fr       */
+/*   Updated: 2024/03/15 02:58:27 by yoelansa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Intern.hpp"
+#include "PresidentialPardonForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include "ShrubberyCreationForm.hpp"
 
 
+Intern::Intern( void ) {
+    FNames[0] = "presidential_pardon_form";
+    FNames[1] = "robotomy_request_form";
+    FNames[2] = "shrubbery_creation_form";
 
-Intern::Intern( const Intern& obj ) {}
-Intern& Intern::operator=( const Intern& obj ) {}
+    form[0] = &Intern::presidential_pardon_form;
+    form[1] = &Intern::robotomy_request_form;
+    form[2] = &Intern::shrubbery_creation_form;
+}
+Intern::Intern( const Intern& obj ) {
+    *this = obj;
+}
+Intern& Intern::operator=( const Intern& obj ) {
+    ( void )obj;
+    return *this;
+}
 Intern::~Intern() {}
 
-AForm* makeForm( std::string Fname, std::string Ftarget ) {
+AForm* Intern::makeForm( std::string name, std::string target ) {
+    for ( int i = 0; i < 3; i++ ) {
+        if (!name.compare(FNames[i]))
+            std::cout << "Intern creates "<< name << std::endl;
+        return (this->*form[i])(target);
+    }
+
+    std::cout << "Intern cannot create " << name << "form." << std::endl;
+    return NULL;
 }
 
+AForm* Intern::presidential_pardon_form (std::string target) {
+    return new PresidentialPardonForm( target );
+}
+
+AForm* Intern::robotomy_request_form (std::string target) {
+    return new RobotomyRequestForm( target );
+}
+
+AForm* Intern::shrubbery_creation_form (std::string target) {
+    return new ShrubberyCreationForm( target );
+}
--- cpp05/ex03/Intern.hpp
+++ cpp05/ex03/Intern.hpp
@@ -6,7 +6,7 @@
 /*   By: yoelansa <yoelansa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/12 21:36:10 by yoelansa          #+#    #+#             */
-/*   Updated: 2024/03/13 16:03:34 by yoelansa         ###   ########.fr       */
+/*   Updated: 2024/03/15 02:55:27 by yoelansa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,18 @@
 #include "Bureaucrat.hpp"
 #include "AForm.hpp"
 
-class Intern : public AForm {
+class Intern {
     private :
-        Intern( void );
+        typedef AForm* (Intern::*t_form)( std::string );
+
+        std::string FNames[3];
+        t_form form[3];
+
+        AForm* presidential_pardon_form( std::string target );
+        AForm* robotomy_request_form( std::string target );
+        AForm* shrubbery_creation_form( std::string target );
     public :
+        Intern( void );
         Intern( const Intern& obj );
         Intern& operator=( const Intern& obj );
         ~Intern();
