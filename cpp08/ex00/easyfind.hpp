d6112ad05522bfd21cc82c5da211c9ef39476e4a
Younes Elansari <yoelansa@e2r6p1.1337.ma>
2024-04-01 12:49:38 -0700

    .

---
+++ cpp08/ex00/easyfind.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yoelansa <yoelansa@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/01 06:51:34 by yoelansa          #+#    #+#             */
+/*   Updated: 2024/04/01 06:56:35 by yoelansa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
+
+#include <iostream>
+
+template <typename T>
+void easyfind( T , int ) {
+    
+}
+
+#endif
